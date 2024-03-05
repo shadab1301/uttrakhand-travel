a38821c7c6df57f1e50963eee8ebe6e9938bca8b
shadab1301 <mdshadab1301@gmail.com>
2024-03-06 01:15:53 +0530

    admin layout implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom';
-import { AdminLayout } from './admin/Layout/AdminLayout';
+import AdminLayout from './admin/Layout/AdminLayout';
 import { UserLayout } from './user/Layout/UserLayout';
 import HomePage from './user/Pages/HomePage';
-import AdminDashboard from './admin/AdminDashboard.jsx';
 import { AuthProvider } from './utils/hook/useAuth.jsx';
 import Login from './admin/Login.jsx';
+import AdminDashboard from './admin/Dashboard/AdminDashboard.jsx';
+import Packages from './admin/Packages/Packages.jsx';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
 
           <Route path="/admin" element={<AdminLayout />}>
             <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="packages" element={<Packages />} />
           </Route>
         </Routes>
       </AuthProvider>
