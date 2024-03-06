249614c40175a5f772fecc860cab42ab9f607791
sandeep325 <negisandeep9819@gmail.com>
2024-03-06 11:27:59 +0530

    commit for contact us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css'
-import Header from './Components/Menu/Header';
-import Footer from './Components/Menu/Footer';
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
 import Home from './Components/Home/Home';
+import { Contact } from './Components/Contact/Contact';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
       <BrowserRouter>
+      <Header />
+
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
       <Footer />
+
+      </BrowserRouter>
 
     </>
   )
