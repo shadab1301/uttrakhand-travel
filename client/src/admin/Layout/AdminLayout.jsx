567b9e948e4ee8af0704b87fa0d7efeb4c5ad39a
shadab1301 <mdshadab1301@gmail.com>
2024-03-16 23:19:33 +0530

    test

--- client/src/admin/Layout/AdminLayout.jsx
+++ client/src/admin/Layout/AdminLayout.jsx
@@ -140,7 +140,7 @@ export default function AdminLayout() {
                   }}
                 >
                   {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                  {menu.icon}
+                   {menu.icon}
                 </ListItemIcon>
                 <ListItemText
                   primary={menu.name}
