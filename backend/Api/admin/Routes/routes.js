12d2fac5b5bf34ccd41ade6fce9ca1e4fc38d646
sandeep325 <negisandeep9819@gmail.com>
2024-03-24 12:54:07 +0530

    commit sandeep

--- backend/Api/admin/Routes/routes.js
+++ backend/Api/admin/Routes/routes.js
@@ -2,8 +2,16 @@ const express = require("express");
 const routes = express.Router();
 const {checkTcken} = require("../auth.middleware");
 const {signupValidation,LoginValidation} = require("../../../Services/Validation/Auth.Validator");
+const {TestimonialValidation} = require("../../../Services/Validation/Common.Validator");
 const {SignUp,Login} = require("../Controller/Admin.Controller");
+const { AddTestimonial, TestimonialList, DeleteTestimonial } = require("../Controller/Testimonial.Controller");
+const {upload1} =require("../../../Services/Common.Services");
+
 
 routes.post('/register',signupValidation,SignUp);
 routes.post('/login',LoginValidation,Login);
+
+routes.post('/testimonial',upload1('testimonial').single('image'),TestimonialValidation,AddTestimonial);
+routes.get('/testimonial',TestimonialList);
+routes.delete('/testimonial/:id',DeleteTestimonial);
 module.exports = routes;
---
+++ backend/Services/Common.Services.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const fs= require("fs");
+
+
+
+exports.upload1 = (url) =>{
+    multer({ dest: "./public/"+url});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/webp') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+        console.log(`file Extension could not supportable. only support extension(.jpge, .png , .jpg)`);
+    }
+}
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { cb(null, './public/' + url); },
+    filename: function (req, file, cb) { cb(null, new Date() / 10 + '_' + file.originalname); }
+});
+const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5 }, fileFilter: fileFilter, });
+
+return upload;
+}
