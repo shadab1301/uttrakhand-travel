cd560ce2a1d725fb52d45cf73b2d234c74e65edf
sandeep325 <negisandeep9819@gmail.com>
2024-04-13 23:34:04 +0530

    commit to reslove a home page banner display issue

--- client/src/user/Layout/Banner.jsx
+++ client/src/user/Layout/Banner.jsx
@@ -4,6 +4,9 @@ import { Link, NavLink, Navigate, Outlet, useLocation } from "react-router-dom";
 import listingimg from "../../../public/images/listing-banner.jpg";
 import detailsimg from "../../../public/images/details-banner.jpg";
 import GalleryBanner from "../../../public/images/listing-banner.jpg";
+import { Slide,Fade,Zoom} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
 export const Banner = () => {
     const location = useLocation();
     const URLPATH = location.pathname.trim().split("/");
@@ -13,10 +16,55 @@ export const Banner = () => {
     console.log(FilertPAth?.[0]);
 
     // FilertPAth.length=9999;
+
+    const spanStyle = {
+        padding: '20px',
+        background: '#efefef',
+        color: '#000000'
+      }
+      
+      const divStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundSize: 'cover',
+        height: '400px'
+      }
+
+      const slideImages = [
+       
+        {
+          url: 'images/slide1.jpg',
+          caption: 'Slide 1'
+        },
+        {
+          url: 'images/slide2.jpg',
+          caption: 'Slide 2'
+        },
+      ];
     const displayMessage = () => {
         if (FilertPAth?.length === 0) {
             return <>
-                <section id="bannerslider">
+
+<div className="slide-container">
+        <Fade autoplay={true}>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                {/* <span >{slideImage.caption}</span> */}
+                <h5>
+                <span style={{fontWeight: "900"}} className="theme-color"> DEVBHOOMI </span>
+                <span style={{fontWeight: "900"}}> TOURS OPERATORS &nbsp;</span>
+                </h5>
+                 <h5 style={{fontWeight: "900"}} className="theme-color">"FEEL THE BEAUTY OF HEAVEN"</h5>
+              </div>
+            </div>
+          ))} 
+        </Fade>
+      </div>
+
+
+                {/* <section id="bannerslider">
                     <div className="owl-carousel owl-theme">
                         <div className="item">
                             <NavLink to="/list/all">
@@ -37,7 +85,19 @@ export const Banner = () => {
                             </NavLink>
                         </div>
                     </div>
-                </section>
+                </section> */}
+
+                {/* <div className="bannercls"> */}
+                    {/* <img className="img-fluid" src={listingimg} alt="" title="" border="0" /> */}
+                    {/* <NavLink to="/list/all">
+                                <div className="banner-txt zoomIn" data-wow-duration="3s">
+                                    <span className="theme-color"> DEVBHOOMI</span> TOURS OPERATORS
+                                    <h5 className="theme-color">FEEL THE BEAUTY OF HEAVEN</h5>
+                                </div>
+                                <img src="images/slide2.jpg" alt="slide2" title="" border="0" />
+                            </NavLink> */}
+                {/* </div> */}
+
             </>
 
         }
